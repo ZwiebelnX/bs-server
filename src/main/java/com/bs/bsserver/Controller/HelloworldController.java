498fc3e0dc45d1fdf1810cff845cce6df02a1bb0
v627222422 <39487849@users.noreply.github.com>
2019-03-23 22:02:11 +0800

    试试

---
+++ src/main/java/com/bs/bsserver/Controller/HelloworldController.java
@@ -0,0 +1,15 @@
+package com.bs.bsserver.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "hello";
+    }
+}
+
+
+

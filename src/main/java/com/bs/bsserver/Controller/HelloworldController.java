1a02761f05c227257495f60a79b5aaefd72ad06a
v627222422 <39487849@users.noreply.github.com>
2019-03-25 23:12:41 +0800

    test

--- src/main/java/com/bs/bsserver/Controller/HelloworldController.java
+++ src/main/java/com/bs/bsserver/Controller/HelloworldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloworldController {
     @RequestMapping("/hello")
     public String index() {
-        return "hello";
+        return "index";
     }
 }
 
